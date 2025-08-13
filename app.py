2d383f656685f3fae72e68bbe4a4040fdc4ae946
imzombier <kona.krishna1997@gmail.com>
2025-08-13 20:10:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ TEMPLATE = """
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>CashinGo</title>
+  <title>CashinGo Easy Payments Gateway By Krishna Naidu</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
